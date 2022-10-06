af316496cc18e9f3f9fd2c8b8de181d8e978751b
dbleske <dbleske@users.noreply.github.com>
2022-10-06 10:48:07 -0500

    Add files via upload

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -6,6 +6,8 @@ return require('packer').startup(function(use)
   use 'itchyny/lightline.vim'
   use 'nvim-treesitter/nvim-treesitter'
   use 'nvie/vim-flake8'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/nvim-cmp'
   -- You can alias plugin names
   use {'joshdick/onedark.vim', as = 'onedark'}
 
