049cff052715006e1a9fd7e2255f292dd68649b6
dbleske <dbleske@users.noreply.github.com>
2022-10-06 10:48:49 -0500

    Add files via upload

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,9 +2,52 @@
 --Plugins and Language Servers
 -------------------------------
 require('plugins')
-require'lspconfig'.pyright.setup{}
+--require'lspconfig'.pyright.setup{}
 --require'lspconfig'.tsserver.setup{}
 --require'lspconfig'.rust_analyzer.setup{}
+
+--------------
+--nvim-cmp setup
+--------------
+local cmp = require'cmp'
+cmp.setup({
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+    end,
+  },
+  window = {
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' }, -- For vsnip users.
+    -- { name = 'luasnip' }, -- For luasnip users.
+    -- { name = 'ultisnips' }, -- For ultisnips users.
+    -- { name = 'snippy' }, -- For snippy users.
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- Set up lspconfig.
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+require('lspconfig')['pyright'].setup {
+  capabilities = capabilities
+}
 
 --------------
 --Basic Setup
@@ -44,7 +87,7 @@ g['netrw_banner'] = 0
 g['netrw_liststyle'] = 3
 g['netrw_browse_split'] = 3
 g['netrw_altv'] = 1
-g['netrw_winsize'] = 20
+--g['netrw_winsize'] = 20
 
 -----------
 --Mappings
